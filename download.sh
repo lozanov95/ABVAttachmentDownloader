5bc354727cbaebf6b43ee4fd9ad2f13ba403269e
Vasil Lozanov <vasillozanov1@gmail.com>
2023-11-13 15:05:21 +0200

    added script that will be used for the preparation of the zips

---
+++ download.sh
@@ -0,0 +1,16 @@
+# Clear from the previous download
+echo "* Clearing the download cache."
+rm -f /tmp/zips/*
+rm -f /downloads/*
+
+# Run the download
+echo "* Starting the attachment download."
+python3 /app/abv_attachment_downloader.py
+
+# Unzip and move the files
+echo "* Extracting the files and zipping them into single archive."
+unzip -P $ZIPPWD "*.zip" 
+zip izvlecheniya.zip *.pdf
+mv izvlecheniya.zip /downloads
+
+echo "* The attachments have been downloaded in Downloads\izvlecheniya."
